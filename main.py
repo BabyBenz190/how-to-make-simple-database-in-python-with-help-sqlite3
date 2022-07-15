f656c49225fa58ff961b9c25cb65fae1ff5b4ce8
BabyBenz <87436964+BabyBenz190@users.noreply.github.com>
2022-07-15 16:01:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import random
 import sqlite3
 import hashlib
-#шифрование пароля алгоритмом с помощью hashlib
+    #шифрование пароля алгоритмом с помощью hashlib
 def md5sum(value):
     return hashlib.md5(value.encode()).hexdigest()
+    #md5sum это функция модуля hashlib
+
+
 
 with sqlite3.connect("database.db") as db:
     cursor = db.cursor()
@@ -28,7 +31,7 @@ with sqlite3.connect("database.db") as db:
     cursor.executescript(query)
     #с помощью метода executescript  мы сможем выполнить несколько запросов одновременно
 
-#запись и регистрация
+    #запись и регистрация
 def registration():
     name = input("Name: ")
     age = int(input("Age: "))
@@ -73,14 +76,16 @@ def log_in():
        db.create_function("md5", 1, md5sum)
 
        cursor.execute("SELECT login FROM users WHERE login = ?", [login])
-       if cursor.fetchone() is None:#если такого логина не существует в нашей таблице
+       if cursor.fetchone() is None:
+        #если такого логина не существует в нашей таблице
           print("Такого логина не существует!")
        else:
             cursor.execute("SELECT password FROM users WHERE login = ? AND password = md5(?)", [login, password])
         #если же логин  существует,то делаем проверку на пароль.
             if cursor.fetchone() is None:
                 print("Пароль неверный!")
-            else:#если пароль верный,то перекидываем пользователя в функцию roulette.
+            else:
+        #если пароль верный,то перекидываем пользователя в функцию roulette.
                 play_roulette(login)
     except sqlite3.Error as e:
         print("Error", e)
@@ -100,16 +105,21 @@ def play_roulette(login):
         if cursor.fetchone() is None:
             print("Вам недостаточно лет!")
         else:
-            bet = int(input("Bet: "))#ставка в рулетке
-            number = random.randint(1, 100)#ставим простой диапазон
+            bet = int(input("Bet: "))
+        #ставка в рулетке
+            number = random.randint(1, 100)
+        #ставим простой диапазон
 
             balance = cursor.execute("SELECT balance FROM users WHERE login = ?", [login]).fetchone()[0]
-            if balance < bet:#если балланс меньше ставки
+            if balance < bet:
+        #если балланс меньше ставки
                 print("Поднимай жопу из-за компьютера и иди работай,а не сливай кэш в рулетку:)")
-            elif balance <=0:#если балланс отрицательный или равен 0,то пользователь также не сможет играть в рулетку.
+            elif balance <=0:
+        #если балланс отрицательный или равен 0,то пользователь также не сможет играть в рулетку.
                 print("Поднимай жопу из-за компьютера и иди работай,а не сливай кэш в рулетку:)")
             else:
-                if number < 50:#если в переменной "number"  получилось число меньше 50,то пользователь проигрывает и деньги переводятся на счет рулетки.
+                if number < 50:
+        #если в переменной "number"  получилось число меньше 50,то пользователь проигрывает и деньги переводятся на счет рулетки.
                     cursor.execute("UPDATE users SET balance - ? WHERE login =? ", [bet, login])
                     cursor.execute("UPDATE roulette SET balance = balance + ?", [bet])
 
